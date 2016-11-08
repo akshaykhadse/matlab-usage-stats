12b20a38221661e7308c357f5145b49c357745cb
Akshay Khadse <akshay621993@gmail.com>
2016-11-08 22:34:13 +0530

    Fixed pep8 issue
    - Fixed pep8 issue (line too long) in migrations file

--- reports/migrations/0001_initial.py
+++ reports/migrations/0001_initial.py
@@ -16,7 +16,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='LogEntry',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True, primary_key=True,
+                                        serialize=False, verbose_name='ID')),
                 ('uid', models.CharField(max_length=50)),
                 ('package', models.CharField(max_length=100)),
                 ('out_time', models.CharField(max_length=8)),
