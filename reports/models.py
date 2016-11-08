481dc0dbc8a9416b09ad871b70fe1f9e4c95e11a
Akshay Khadse <akshay621993@gmail.com>
2016-11-08 22:07:51 +0530

    Added __str__ method, test for LogEntry
    - Added __str__ method for LogEntry
    - Updated LogEntry to include remining attributes
    - Remade migrations

--- reports/models.py
+++ reports/models.py
@@ -5,3 +5,10 @@ class LogEntry(models.Model):
     uid = models.CharField(max_length=50)
     package = models.CharField(max_length=100)
     out_time = models.CharField(max_length=8)
+    emp_number = models.CharField(max_length=100)
+    emp_type = models.CharField(max_length=100)
+    department = models.CharField(max_length=100)
+    in_time = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.out_time + ' ' + self.package
--- reports/tests.py
+++ reports/tests.py
@@ -6,8 +6,18 @@ class Test_reports_models(TestCase):
     def test_trivial_LogEntry_creation(self):
         entry = LogEntry()
         self.assertTrue(isinstance(entry, LogEntry))
+        self.assertEqual(entry.__str__(), entry.out_time + ' ' + entry.package)
 
     def test_str_metdod_LogEntry(self):
         entry = LogEntry(uid='testuid1', package='sample matlab package',
-                         out_time='13:05:57')
+                         out_time='13:05:57', emp_number='123456789',
+                         emp_type='xx', department='DEPT', in_time='22:10:16')
         self.assertTrue(isinstance(entry, LogEntry))
+        self.assertEqual(entry.__str__(), entry.out_time + ' ' + entry.package)
+
+    def test_save_objects_LogEntry(self):
+        LogEntry.objects.all().delete()
+        entry = LogEntry(uid='testuid2')
+        entry.save()
+        del entry
+        self.assertEqual(len(LogEntry.objects.filter(uid='testuid2')), 1)
